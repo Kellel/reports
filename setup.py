cd86491869f42c58af06cdc89c8be9966f0476ff
Kellen Fox <kellen@cablespeed.com>
2015-07-21 23:57:10 -0700

    tweaked setup script

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
-from distutils.core import setup
+from setuptools import setup
+from pip.req import parse_requirements
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements('requirements.txt')
+
+# reqs is a list of requirement
+# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(name='reports',
       version='1.0',
@@ -6,5 +14,6 @@ setup(name='reports',
       author="Kellen Fox",
       author_email='kellen@cablespeed.com',
       packages=['report'],
+      install_requires=reqs,
       scripts=['scripts/report-daemon']
 )
