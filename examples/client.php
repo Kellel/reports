09659356ba55b251180f9159f497019e19c26bbc
Kellen Fox <kellen@cablespeed.com>
2015-08-05 19:23:29 -0700

    make client do work

--- examples/client.php
+++ examples/client.php
@@ -1,9 +1,21 @@
+#!/usr/bin/env php
 <?php
 
+function usage($argv) {
+    echo "Send a job to the report daemon\n";
+    echo "Usage: php client.php FILENAME\n";
+}
+
+if ($argc != 2) {
+    usage($argv);
+    return;
+};
+
+$filename = $argv[1];
+
+$data = array("filename" => $filename, "user" => "testuser");
+
 $redis = new Redis();
-
-$data = array("filename" => "testfile.txt", "user" => "testuser");
-
 $redis->connect('127.0.0.1', 6379);
 $redis->lPush("report-listen-queue", json_encode($data));
 
